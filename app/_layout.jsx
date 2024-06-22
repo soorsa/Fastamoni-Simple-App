b4a7d83200b1c3ea586098ba515479d0dbf71294
soorsa <kingsoorsa@gmail.com>
2024-06-22 21:31:50 +0100

    first commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,57 +8,12 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
 import { useColorScheme } from "@/hooks/useColorScheme";
-
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
-import BottomNavBar from "@/navigation/BottomNavBar";
 import { registerRootComponent } from "expo";
-
-import { View, Text } from "react-native";
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "../screens/Home";
-import LoginPage from "../screens/LoginPage";
-import Profile from "../screens/Profile";
-import { Ionicons } from "@expo/vector-icons";
 
-const Tab = createBottomTabNavigator();
-const screenOptions = {
-  tabBarShowLabel: false,
-  tabBarHideOnKeyboard: true,
-  headerShown: false,
-  // tabBarStyle: {
-  //   position: "absolute",
-  //   height: 70,
-  //   width: "95%",
-  //   backgroundColor: "chocolate",
-  //   elevation: 5,
-  //   flex: 1,
-  //   justifyContent: "center", // center vertically
-  //   alignItems: "center",
-  //   left: 0,
-  //   bottom: 20,
-  //   right: 0,
-  //   // marginHorizontal: "auto",
-  //   borderRadius: 10,
-  // },
-  tabBarStyle: {
-    position: "absolute",
-    height: 70,
-    width: "95%",
-    backgroundColor: "chocolate",
-    elevation: 5,
-    left: "2.5%", // Adjust left positioning to center horizontally
-    bottom: 20,
-    borderRadius: 10,
-    borderColor: "chocolate",
-    // flexDirection: "row", // Set flexDirection to row to use justifyContent and alignItems
-    // justifyContent: "center", // Center horizontally
-    // alignItems: "center", // Center vertically
-  },
-};
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
